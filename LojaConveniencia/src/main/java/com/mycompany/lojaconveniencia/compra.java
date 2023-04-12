45c561196f8e54bb887a82beeb5b3241a3980085
MayconFelipeBianchi <114965402+MayconFelipeBianchi@users.noreply.github.com>
2023-04-11 22:55:42 -0300

    Classe Compra
    adicionando pela segunda vez

--- LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/compra.java
+++ LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/compra.java
@@ -9,5 +9,29 @@ package com.mycompany.lojaconveniencia;
  * @author Usuario
  */
 public class compra {
-    
+    public int idCompra; 
+    public String fornecedor; 
+    public int numNota;
+    public int datEmicao;
+    public int datEntrada;
+    public double valTotalProd;
+    public double valorAdicional;
+    public double valorDesc;
+    public double valorNota;
+    public int codProd;
+    public double valorUn;
+       
+    public double compras (int idCompra,String fornecedor,double valTotalProd,double valorAdicional){
+        double compra = 0;
+        return compra;
+    }
+    public void listCompra (int idCompra){
+        int listCompra;
+    }
+    public void aterarCompra (int idCompra){
+        double aterarCompra = 0;
 }
+    public void getCompra(int idCompra){
+        double getCompra = 0;
+    }
+    }
