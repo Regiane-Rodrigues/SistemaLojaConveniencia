8b14dbe45d592b857ac10d7b696d13c2dd6c7709
Gustavo H. Nava <navahgustavo@gmail.com>
2023-06-07 20:52:49 -0300

    Update Fornecedor.java
    Adição do construtor

--- LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/Fornecedor.java
+++ LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/Fornecedor.java
@@ -8,6 +8,12 @@ public class Fornecedor extends pessoaJuridica{
     
     private String vendedor;
     
+    public Fornecedor(String nomeCompleto, int telefone, String email, int cep, String cidade, String estado, String rua, int numeroLocal, String cnpj, String nomeEmpresa, String setorAtuacao, String vendedor) {
+        super(nomeCompleto, telefone, email, cep, cidade, estado, rua, numeroLocal, cnpj, nomeEmpresa, setorAtuacao);
+        
+        this.vendedor = vendedor;
+    }
+    
     @Override
     public void exibirInformacoes(){
     
