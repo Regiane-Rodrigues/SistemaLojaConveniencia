603c6d80216821a4515e6635987ab76236e76585
Gustavo H. Nava <navahgustavo@gmail.com>
2023-06-13 20:33:13 -0300

    Reversão

--- LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/CadastroBanco.java
+++ LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/CadastroBanco.java
@@ -9,9 +9,9 @@ import java.util.List;
  * @author Gustavo H. Nava
  */
 public class CadastroBanco {
-    
+
     List<Banco> listaDeBancos = new ArrayList<>();
-    
+
     public void adicionar() {
         Banco novoBanco = new Banco();
         System.out.println("|| CADASTRO DE BANCOS ||\n");
@@ -36,39 +36,48 @@ public class CadastroBanco {
         listaDeBancos.add(novoBanco);
         System.out.println("\nBanco cadastrado com sucesso\n");
     }
-    
+
     public void editar() {
         System.out.println("|| EDIÇÃO DE CADASTRO DE BANCOS ||\n");
         System.out.println("Informe o ID do Banco a ser editado: ");
         int idBusca = inserir.nextInt();
         boolean idEncontrado = false;
-        
+
         for (int i = 0; i < listaDeBancos.size(); i++) {
             if (idBusca == listaDeBancos.get(i).idBanco) {
-                idEncontrado = true;
-                System.out.println("Informe o CNPJ do Banco: ");
-                inserir.nextLine();
-                listaDeBancos.get(i).cnpjBanco = inserir.nextLine();
-                System.out.println("Informe o NOME do Banco: ");
-                listaDeBancos.get(i).nomeEmpresa = inserir.nextLine();
-                System.out.println("Informe o CEP do Banco: ");
-                listaDeBancos.get(i).cep = inserir.nextLine();
-                System.out.println("Informe o ESTADO do Banco: ");
-                listaDeBancos.get(i).estado = inserir.nextLine();
-                System.out.println("Informe a CIDADE do Banco: ");
-                listaDeBancos.get(i).cidade = inserir.nextLine();
-                System.out.println("Informe o TELEFONE do Banco: ");
-                listaDeBancos.get(i).numero = inserir.nextLine();
-                System.out.println("\nBanco editado com sucesso\n");
-                break;
+                System.out.println("Cadastro encontrado: \n" + listaDeBancos.get(i).toString());
+                System.out.println("Confirmar edicao? (S)");
+                char confirmaEdicao = inserir.next().charAt(0);
+                confirmaEdicao = Character.toUpperCase(confirmaEdicao);
+                if (confirmaEdicao == 'S') {
+                    idEncontrado = true;
+                    System.out.println("Informe o CNPJ do Banco: ");
+                    inserir.nextLine();
+                    listaDeBancos.get(i).cnpjBanco = inserir.nextLine();
+                    System.out.println("Informe o NOME do Banco: ");
+                    listaDeBancos.get(i).nomeEmpresa = inserir.nextLine();
+                    System.out.println("Informe o CEP do Banco: ");
+                    listaDeBancos.get(i).cep = inserir.nextLine();
+                    System.out.println("Informe o ESTADO do Banco: ");
+                    listaDeBancos.get(i).estado = inserir.nextLine();
+                    System.out.println("Informe a CIDADE do Banco: ");
+                    listaDeBancos.get(i).cidade = inserir.nextLine();
+                    System.out.println("Informe o TELEFONE do Banco: ");
+                    listaDeBancos.get(i).numero = inserir.nextLine();
+                    System.out.println("\nBanco editado com sucesso\n");
+                    break;
+                } else {
+                    System.out.println("Cancelando exclusão.");
+                    break;
+                }
             }
         }
-        
+
         if (!idEncontrado) {
             System.out.println("ID não encontrado.\n");
         }
     }
-    
+
     public void listar() {
         System.out.println("|| BANCOS CADASTRADOS ||\n");
         if (listaDeBancos.isEmpty()) {
@@ -79,22 +88,31 @@ public class CadastroBanco {
             }
         }
     }
-    
+
     public void excluir() {
         System.out.println("|| EXCLUSÃO DE CADASTRO DE BANCOS ||\n");
         System.out.println("Informe o ID do Banco a ser excluído: ");
         int idBusca = inserir.nextInt();
         boolean idEncontrado = false;
-        
+
         for (int i = 0; i < listaDeBancos.size(); i++) {
             if (idBusca == listaDeBancos.get(i).idBanco) {
-                idEncontrado = true;
-                listaDeBancos.remove(listaDeBancos.get(i));
-                System.out.println("\nBanco excluído com sucesso\n");
-                break;
+                System.out.println("Cadastro encontrado: \n" + listaDeBancos.get(i).toString());
+                System.out.println("Confirmar exclusão? (S)");
+                char confirmaExclusao = inserir.next().charAt(0);
+                confirmaExclusao = Character.toUpperCase(confirmaExclusao);
+                if (confirmaExclusao == 'S') {
+                    idEncontrado = true;
+                    listaDeBancos.remove(listaDeBancos.get(i));
+                    System.out.println("\nBanco excluído com sucesso\n");
+                    break;
+                } else {
+                    System.out.println("Cancelando exclusão.");
+                    break;
+                }
             }
         }
-        
+
         if (!idEncontrado) {
             System.out.println("ID não encontrado.\n");
         }
--- LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/LojaConveniencia.java
+++ LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/LojaConveniencia.java
@@ -9,149 +9,156 @@ import java.util.Scanner;
  */
 public class LojaConveniencia {
 
-	static Scanner inserir = new Scanner(System.in);
+    static Scanner inserir = new Scanner(System.in);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int escolhaMenu = -1;
+        int escolhaMenu = -1;
 
-		System.out.println("-----------------------------------------");
-		System.out.println(" || Sistema Loja de Conveniêcnia ||");
+        System.out.println("-----------------------------------------");
+        System.out.println(" || Sistema Loja de Conveniêcnia ||");
 
+        do {
+            System.out.println("-----------------------------------------");
+            System.out.println("Escolha a opção desejada.");
+            System.out.println("1 - Cadastro Pessoa");
+            System.out.println("2 - Cadastro Produto");
+            System.out.println("3 - Compras");
+            System.out.println("4 - Vendas");
+            System.out.println("5 - Estoque");
+            System.out.println("6 - Formas de pagamento");
+            System.out.println("7 - Finanças");
+            System.out.println("-----------------------------------------");
+            if (inserir.hasNextInt()) {
+                escolhaMenu = inserir.nextInt();
 
-		do {
-			System.out.println("-----------------------------------------");
-			System.out.println("Escolha a opção desejada.");
-			System.out.println("1 - Cadastro Pessoa");
-			System.out.println("2 - Cadastro Produto");
-			System.out.println("3 - Compras");
-			System.out.println("4 - Vendas");
-			System.out.println("5 - Estoque");
-			System.out.println("6 - Formas de pagamento");
-			System.out.println("7 - Finanças");
-			System.out.println("-----------------------------------------");
-		if (inserir.hasNextInt()) {
-				escolhaMenu = inserir.nextInt();
+                switch (escolhaMenu) {
+                    case 1:
+                        menuCadastroPessoa();
+                        break;
+                    case 2:
+                        menuCadastroProduto();
+                        break;
+                    case 3:
+                        menuCompra();
+                        break;
+                    case 4:
+                        menuVenda();
+                        break;
+                    case 5:
+                        menuEstoque();
+                        break;
+                    case 6:
+                        menuPagamento();
+                        break;
+                    case 7:
+                        menuFinancas();
+                        break;
 
-
-				switch (escolhaMenu) {
-				case 1:
-					menuCadastroPessoa();
-					break;
-				case 2:
-					menuCadastroProduto();
-					break;
-				case 3:
-					menuCompra();
-					break;
-				case 4:
-					menuVenda();
-					break;
-				case 5:
-					menuEstoque();
-					break;
-				case 6:
-					menuPagamento();
-					break;
-				case 7:
-					menuFinanceiro();
-					int setor = -1;
-					// Arrumar, está entrando em loop por motivos de: Não sei!
-					System.out.println("|| ESCOLHA UM SETOR ||");
-					do {
-						System.out.println("1 - Financeiro");
-						System.out.println("2 - Fiscal");
-						System.out.println("0 - Retornar ao Menu Principal");
-
-						if (inserir.hasNextInt()) {
-							setor = inserir.nextInt();
-							switch (setor) {
-							case 1:
-								menuFinanceiro();
-								break;
-							case 2:
-								menuFiscal();
-								break;
-							case 0:
-								System.out.println("Sistema Encerrado");
-								break;
-							default:
-								System.out.println("Digite uma opção valida para continuar.");
-								break;
-							}
-						} else {
-							System.out.println("Opção Inválida.");
-							inserir.nextInt(); // impede que o menu entre em loop infinito
-						}
-					} while (setor != 0);
-					break;
-
-				case 0:
-					System.out.println("Obrigado Volte Sempre!");
-					break;
-				default:
-					System.out.println("Opção inválida. Favor informar outra opção.");
-					break;
-				}
-			} else {
-				System.out.println("Opção inválida. Favor informar outra opção.");
-				inserir.next(); // favor não apagar garante que o menu não entre em loop infinito caso uma
-								// informação errada seja passada no scanner
-			}
-		} while (escolhaMenu != 0);
-	}
-
-	public static void menuCadastroPessoa() {
-
-		cadastroPessoa Pessoa = new cadastroPessoa();
-		Pessoa.menuPessoa();
-	}
-
-	public static void menuCadastroProduto() {
-
-		cadastroProduto Produto = new cadastroProduto();
-		Produto.menuProduto();
-	}
-
-	public static void menuCompra() {
-
-		compra Compra = new compra();
-		Compra.exibirMenu();
-	}
-
-	public static void menuVenda() {
-
-		venda Vendas = new venda();
-		Vendas.menuVendas();
-	}
-
-	public static void menuEstoque() {
-            
-            System.out.println("Testando");
-		//Estoque incluirEstoque = new Estoque();
-		//incluirEstoque.menuEstoques();
-	}
-
-	public static void menuPagamento() {
-
-		Pagamento formaPagamento = new Pagamento();
-		formaPagamento.menuPagamento();
-	}
-        
+                    case 0:
+                        System.out.println("Obrigado Volte Sempre!");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Favor informar outra opção.");
+                        break;
+                }
+            } else {
+                System.out.println("Opção inválida. Favor informar outra opção.");
+                inserir.next(); // favor não apagar garante que o menu não entre em loop infinito caso uma
+                // informação errada seja passada no scanner
+            }
+        } while (escolhaMenu != 0);
+    }
     
+    public static void menuFinancas() {
 
-	public static void menuFinanceiro() {
+        int escolhaSetor = -1;
 
-		Financeiro financeiro = new Financeiro();
-		financeiro.menuFinanceiro();
-	}
+        System.out.println("-----------------------------------------");
 
-	public static void menuFiscal() {
+        System.out.println("|| ESCOLHA UM SETOR ||");
+        do {
+            System.out.println("-----------------------------------------");
+            System.out.println("1 - Financeiro");
+            System.out.println("2 - Fiscal");
+            System.out.println("3 - Banco");
+            System.out.println("0 - Retornar ao Menu Principal");
+            System.out.println("-----------------------------------------");
+            if (inserir.hasNextInt()) {
+                escolhaSetor = inserir.nextInt();
+                switch (escolhaSetor) {
+                    case 1:
+                        menuFinanceiro();
+                        break;
+                    case 2:
+                        menuFiscal();
+                        break;
+                    case 3:
+                        menuBanco();
+                        break;
+                    case 0:
+                        System.out.println("Voltando.");
+                        break;
+                    default:
+                        System.out.println("Digite uma opção valida para continuar.");
+                        break;
+                }
+            } else {
+                System.out.println("Opção Inválida.");
+                inserir.nextInt(); //impede que o menu entre em loop infinito
+            }
+        } while (escolhaSetor != 0);
+    }
 
-		System.out.println("Essa classe ainda não funciona");
-	}
+    public static void menuCadastroPessoa() {
 
-	public static void menuBanco() {
+        cadastroPessoa Pessoa = new cadastroPessoa();
+        Pessoa.menuPessoa();
+    }
+
+    public static void menuCadastroProduto() {
+
+        cadastroProduto Produto = new cadastroProduto();
+        Produto.menuProduto();
+    }
+
+    public static void menuCompra() {
+
+        compra Compra = new compra();
+        Compra.exibirMenu();
+    }
+
+    public static void menuVenda() {
+
+        venda Vendas = new venda();
+        Vendas.menuVendas();
+    }
+
+    public static void menuEstoque() {
+
+        System.out.println("Testando");
+        //Estoque incluirEstoque = new Estoque();
+        //incluirEstoque.menuEstoques();
+    }
+
+    public static void menuPagamento() {
+
+        Pagamento formaPagamento = new Pagamento();
+        formaPagamento.menuPagamento();
+    }
+
+    public static void menuFinanceiro() {
+
+        Financeiro financeiro = new Financeiro();
+        financeiro.menuFinanceiro();
+    }
+
+    public static void menuFiscal() {
+
+        System.out.println("Essa classe ainda não funciona");
+    }
+
+    public static void menuBanco() {
         MenuBanco banco = new MenuBanco();
         banco.menuBancos();
     }
